4c269d9302efc574551ea089c221c6139e2e7067
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-05-11 13:38:55 +0100

    load color change

--- components/Load.vue
+++ components/Load.vue
@@ -13,24 +13,24 @@
   width: 16px;
   height: 16px;
   border-radius: 50%;
-  background-color: #fff;
-  box-shadow: 32px 0 #fff, -32px 0 #fff;
+  background-color: #121212;
+  box-shadow: 32px 0 #121212, -32px 0 #121212;
   position: relative;
   animation: flash 0.5s ease-out infinite alternate;
 }
 
 @keyframes flash {
   0% {
-    background-color: #FFF2;
-    box-shadow: 32px 0 #FFF2, -32px 0 #FFF;
+    background-color: #444444;
+    box-shadow: 32px 0 #444444, -32px 0 #121212;
   }
   50% {
-    background-color: #FFF;
-    box-shadow: 32px 0 #FFF2, -32px 0 #FFF2;
+    background-color: #121212;
+    box-shadow: 32px 0 #444444, -32px 0 #444444;
   }
   100% {
-    background-color: #FFF2;
-    box-shadow: 32px 0 #FFF, -32px 0 #FFF2;
+    background-color: #444444;
+    box-shadow: 32px 0 #121212, -32px 0 #444444;
   }
 }
       
